ed7a4e45e5d0a8f7bc4c3ba6b5e06787ac5b6f45
Unai Elordi Hidalgo <uelordi@vicomtech.org>
2017-10-09 10:40:18 +0200

    adding base dlib project to the repo

---
+++ Dlib-deep-sample/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,42 @@
+
+#include <string>
+#include "native-lib.h"
+//#include <opencv2/core.hpp>
+#include "opencv2/opencv.hpp"
+#include "fps.h"
+
+//OpenCV includes:
+Fps *fpsCounter;
+JNIEXPORT void JNICALL
+Java_org_opencv_samples_captureonly_JniManager_init(JNIEnv *env, jclass type) {
+
+    // TODO
+}
+JNIEXPORT void JNICALL
+Java_org_opencv_samples_captureonly_JniManager_process(JNIEnv *env, jclass type, jlong colorImage,
+                                                       jlong greyImage) {
+
+    // TODO
+    cv::Mat  &inMat =*(cv::Mat *) colorImage;
+    float fps = fpsCounter->checkFps();
+    std::stringstream ss;
+    ss.precision(4);
+    ss << "FPS "<< fps;
+    cv::putText(inMat,ss.str().c_str(), cv::Point(15,15),  CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0,255,0));
+}
+
+JNIEXPORT void JNICALL
+Java_org_opencv_samples_captureonly_JniManager_start(JNIEnv *env, jclass type) {
+
+    // TODO
+    fpsCounter = new Fps();
+    fpsCounter->start();
+
+}
+
+JNIEXPORT void JNICALL
+Java_org_opencv_samples_captureonly_JniManager_stop(JNIEnv *env, jclass type) {
+
+    // TODO
+
+}
